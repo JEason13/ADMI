c3ee5668c4d0bf9a8d34a66e46c710a9247031ca
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2023-02-09 09:43:41 -0500

    Update ADMI.ps1

--- ADMI.ps1
+++ ADMI.ps1
@@ -247,7 +247,8 @@ function ADMI_AppWindow {
     
     if ($CheckCommandBox -eq [System.Windows.Forms.DialogResult]::Yes){
         #needs to be tested on an Active Directory system 
-        Get-ADUser + " " + $GetADUserFromGUI 
+        #[string]$GetTheADUser = $GetADUserFromGUI.ToString()
+        Get-ADUser -Filter 'sAMAccountName -eq $GetADUserFromGUI' #https://stackoverflow.com/questions/20075502/get-aduser-filter-will-not-accept-a-variable
         ADMI_AppWindow
     }
 
